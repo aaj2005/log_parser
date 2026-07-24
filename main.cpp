b07518573e2519c1df0503adc4177c0308abb607
Ali Aljishi <ali.aljishi.2005@gmail.com>
2026-07-24 16:32:08 +0100

    feat: main file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <vector>
 #include "log_parser.cpp"
 
 
 
-int main(){
+int main(int argc, char* argv[]){
 
+	if (argc < 2){
+		std::cerr << "Insufficient args passed!"<<std::endl;
+		return -1;
+	}
+	std::vector<std::string> files;
+	files.reserve(argc);
+	for (int i = 1; i < argc; i++) {
+		std::cout<<argc<<std::endl;
+		if(!std::filesystem::is_regular_file(argv[i])){
+			std::cerr << "Argument:"<< argv[i]<<" is not a file!"<<std::endl;
+			return -1;
+		}
+		files.push_back(argv[i]);
+    }
 	
+	LogParser parser = LogParser(files);
+	parser.parse_all();
 
 	return 0;
 }
